0374ee06e7141f5dd97ce1669c1892d9fe4aa7f4
DownsideupmediaUSA <brandondecarlo@live.com>
2017-01-04 13:53:10 -0500

    Done.

--- js/app/controllers/MainController.js
+++ js/app/controllers/MainController.js
@@ -2,6 +2,10 @@ function MainController() {
     this.name = 'Bill Gates';
 }
 
+function addNumbers(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+
 angular
     .module('app')
     .controller('MainController', MainController);
